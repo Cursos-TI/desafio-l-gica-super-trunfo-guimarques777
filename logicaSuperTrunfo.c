1f153fa33618b5fb47a781fd858ffa1eb88456e6
guimarques777 <guiguidex6@gmail.com>
2025-08-06 11:04:16 +0000

    desafio nivel mestre

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,6 +1,7 @@
+
 #include <stdio.h>
 
-int main(){
+int main() {
 //variáveis para armazenar os dados da primeira carta.
 char estado1[3];
 char codigo1[5];
@@ -24,7 +25,9 @@ int pontosTuristicos2;
 float densidade2;
 float ppc2;
 float superpoder2;
-int atributo;
+int atributo1, atributo2;
+float soma_carta1 = 0;
+float soma_carta2 = 0;
 
 
 printf("Você ira usar 2 cartas nessa rodada.\n");
@@ -115,94 +118,225 @@ printf("Superpoder: %.2f\n", superpoder2);
 
 //comparação de atributos das cartas.
 
-printf("Digite o numero de acordo com o atributo que deseja comparar entre as cartas (população, área, pib, pontos turísticos, densidade populacional):\n");
+printf("Escolha 2 atributos para comparar na batalha entre as cartas:\n");
 printf("1. população\n");
 printf("2. área\n");
 printf("3. pib\n");
 printf("4. pontos turísticos\n");
 printf("5. densidade populacional\n");
 
-scanf("%d", &atributo);
+printf("Digite o número do atributo que deseja comparar (1 a 5):\n ");
+scanf(" %d", &atributo1);
+printf("Agora, escolha outro atributo para comparar:\n");
+printf("Atenção: não é possível escolher o mesmo atributo duas vezes.\n");
+printf("Digite o número do segundo atributo que deseja comparar (1 a 5):\n ");
+scanf(" %d", &atributo2);
 
-switch (atributo)
-{
-case 1:
+if (atributo1 == atributo2) {
+    printf("Você não pode escolher o mesmo atributo duas vezes. Tente novamente.\n");
+    return 1; // Encerra o programa se os atributos forem iguais
+}
+
+
+
+switch (atributo1) {
+     case 1:
+    soma_carta1 += (float)populacao1;
+        soma_carta2 += (float)populacao2;
+    printf("primeiro atributo escolhido:\n");
+    printf("====Batalha Individual====\n");
     printf("Estado %s X Estado %s\n", estado1, estado2);
     printf("atributo usado para comparação: População\n");
     printf("População da carta 1: %lu\n", populacao1);
     printf("População da carta 2: %lu\n", populacao2);
     if(populacao1 > populacao2) {
-        printf("A carta 1 venceu.\n");
+        printf("A carta 1 venceu na batalha individual.\n");
     } else if(populacao1 < populacao2) {
-        printf("A carta 2 venceu.\n");
-    } else {
-        printf("Empate.\n");
+        printf("A carta 2 venceu na batalha individual.\n");
+    }else {
+        printf("As cartas empataram na batalha individual.\n");
     }
 
-    break;
-case 2:
-    printf("Estado %s X Estado %s\n", estado1, estado2); 
+
+break;
+    
+     case 2: 
+        soma_carta1 += area1;
+        soma_carta2 += area2;
+    printf("primeiro atributo escolhido:\n");
+    printf("====Batalha Individual====\n");
+    printf("Estado %s X Estado %s\n", estado1, estado2);
     printf("atributo usado para comparação: Área\n");
     printf("Área da carta 1: %.2f Km²\n", area1);
     printf("Área da carta 2: %.2f Km²\n", area2);
     if(area1 > area2) {
-        printf("A carta 1 venceu.\n");
+        printf("A carta 1 venceu na batalha individual.\n");
     } else if(area1 < area2) {
-        printf("A carta 2 venceu.\n");
+        printf("A carta 2 venceu na batalha individual.\n");
     } else {
-        printf("Empate.\n");
+        printf("As cartas empataram na batalha individual.\n");
     }
-    break;
-case 3:
+     
+        break;
+    case 3:
+        soma_carta1 += pib1;
+        soma_carta2 += pib2;
+    printf("primeiro atributo escolhido:\n");
+    printf("====Batalha Individual====\n");
     printf("Estado %s X Estado %s\n", estado1, estado2);
     printf("atributo usado para comparação: PIB\n");
     printf("PIB da carta 1: R$ %.2lf\n", pib1);
     printf("PIB da carta 2: R$ %.2lf\n", pib2);
     if(pib1 > pib2) {
-        printf("A carta 1 venceu.\n");
+        printf("A carta 1 venceu na batalha individual.\n");
     } else if(pib1 < pib2) {
-        printf("A carta 2 venceu.\n");
+        printf("A carta 2 venceu na batalha individual.\n");
     } else {
-        printf("Empate.\n");
+        printf("As cartas empataram na batalha individual.\n");
     }
-    break;
-case 4:
+        break;
+    case 4:
+        soma_carta1 += (float)pontosTuristicos1;
+        soma_carta2 += (float)pontosTuristicos2;
+    printf("primeiro atributo escolhido:\n");
+    printf("====Batalha Individual====\n");
     printf("Estado %s X Estado %s\n", estado1, estado2);
     printf("atributo usado para comparação: Pontos Turísticos\n");
     printf("Pontos Turísticos da carta 1: %d\n", pontosTuristicos1);
     printf("Pontos Turísticos da carta 2: %d\n", pontosTuristicos2);
     if(pontosTuristicos1 > pontosTuristicos2) {
-        printf("A carta 1 venceu.\n");
+        printf("A carta 1 venceu na batalha individual.\n");
     } else if(pontosTuristicos1 < pontosTuristicos2) {
-        printf("A carta 2 venceu.\n");
+        printf("A carta 2 venceu na batalha individual.\n");
     } else {
-        printf("Empate.\n");
-    }break;
-case 5:
-    printf("Estado %s X Estado %s\n", estado1, estado2); 
+        printf("As cartas empataram na batalha individual.\n");
+    }
+        break;
+    case 5:
+    
+        soma_carta1 += (1.0f / densidade1);  // quanto menor a densidade, maior o valor somado
+        soma_carta2 += (1.0f / densidade2);
+    printf("primeiro atributo escolhido:\n");
+    printf("====Batalha Individual====\n");
+    printf("Estado %s X Estado %s\n", estado1, estado2);
     printf("atributo usado para comparação: Densidade Populacional\n");
     printf("Densidade Populacional da carta 1: %.2f hab/Km²\n", densidade1);
     printf("Densidade Populacional da carta 2: %.2f hab/Km²\n", densidade2);
-    if(densidade1 < densidade2) {
-        printf("A carta 1 venceu.\n");
+
+    if(densidade1 < densidade2) { // quanto menor a densidade, maior o valor somado
+        printf("A carta 1 venceu na batalha individual.\n");
     } else if(densidade1 > densidade2) {
-        printf("A carta 2 venceu.\n");
+        printf("A carta 2 venceu na batalha individual.\n");
     } else {
-        printf("Empate.\n");
+        printf("As cartas empataram na batalha individual.\n");
     }
-    break;
-default:
-    printf("Atributo inválido! Por favor, escolha um número de 1 a 5.\n");
-    break;
+        break;
+    default:
+        printf("Atributo inválido! Por favor, escolha um número entre 1 e 5.\n");
+        return 1; // Encerra o programa se o atributo for inválido
+    }
+// Soma do segundo atributo
+switch (atributo2) {
+    case 1:
+        soma_carta1 += (float)populacao1;
+        soma_carta2 += (float)populacao2;
+        printf("Segundo atributo escolhido:\n");
+        printf("====Batalha Individual====\n");
+        printf("Estado %s X Estado %s\n", estado1, estado2);
+        printf("atributo usado para comparação: População\n");
+        printf("População da carta 1: %lu\n", populacao1);
+        printf("População da carta 2: %lu\n", populacao2);
+        if(populacao1 > populacao2) {
+            printf("A carta 1 venceu na batalha individual.\n");
+        } else if(populacao1 < populacao2) {
+            printf("A carta 2 venceu na batalha individual.\n");
+        } else {
+            printf("As cartas empataram na batalha individual.\n");
+        }
+        break;
+    case 2:
+        soma_carta1 += area1;
+        soma_carta2 += area2;
+        printf("Segundo atributo escolhido:\n");
+        printf("====Batalha Individual====\n");
+        printf("Estado %s X Estado %s\n", estado1, estado2);
+        printf("atributo usado para comparação: Área\n");
+        printf("Área da carta 1: %.2f Km²\n", area1);
+        printf("Área da carta 2: %.2f Km²\n", area2);
+        if(area1 > area2) {
+            printf("A carta 1 venceu na batalha individual.\n");
+        } else if(area1 < area2) {
+            printf("A carta 2 venceu na batalha individual.\n");
+        } else {
+            printf("As cartas empataram na batalha individual.\n");
+        }
+        break;
+    case 3:
+        soma_carta1 += pib1;
+        soma_carta2 += pib2;
+        printf("Segundo atributo escolhido:\n");
+        printf("====Batalha Individual====\n");
+        printf("Estado %s X Estado %s\n", estado1, estado2);
+        printf("atributo usado para comparação: PIB\n");
+        printf("PIB da carta 1: R$ %.2lf\n", pib1);
+        printf("PIB da carta 2: R$ %.2lf\n", pib2);
+        if(pib1 > pib2) {
+            printf("A carta 1 venceu na batalha individual.\n");
+        } else if(pib1 < pib2) {
+            printf("A carta 2 venceu na batalha individual.\n");
+        } else {
+            printf("As cartas empataram na batalha individual.\n");
+        }
+        break;
+    case 4:
+        soma_carta1 += (float)pontosTuristicos1;
+        soma_carta2 += (float)pontosTuristicos2;
+        printf("Segundo atributo escolhido:\n");
+        printf("====Batalha Individual====\n");
+        printf("Estado %s X Estado %s\n", estado1, estado2);
+        printf("atributo usado para comparação: Pontos Turísticos\n");
+        printf("Pontos Turísticos da carta 1: %d\n", pontosTuristicos1);
+        printf("Pontos Turísticos da carta 2: %d\n", pontosTuristicos2);
+        if(pontosTuristicos1 > pontosTuristicos2) {
+            printf("A carta 1 venceu na batalha individual.\n");
+        } else if(pontosTuristicos1 < pontosTuristicos2) {
+            printf("A carta 2 venceu na batalha individual.\n");
+        } else {
+            printf("As cartas empataram na batalha individual.\n");
+        }
+        break;
+    case 5:
+        soma_carta1 += (1.0f / densidade1);
+        soma_carta2 += (1.0f / densidade2);
+        printf("Segundo atributo escolhido:\n");
+        printf("====Batalha Individual====\n");
+        printf("Estado %s X Estado %s\n", estado1, estado2);
+        printf("atributo usado para comparação: Densidade Populacional\n");
+        printf("Densidade Populacional da carta 1: %.2f hab/Km²\n", densidade1);
+        printf("Densidade Populacional da carta 2: %.2f hab/Km²\n", densidade2);
+        if(densidade1 < densidade2) {
+            printf("A carta 1 venceu na batalha individual.\n");
+        } else if(densidade1 > densidade2) {
+            printf("A carta 2 venceu na batalha individual.\n");
+        } else {
+            printf("As cartas empataram na batalha individual.\n");
+        }
+        break;
+    default:
+        printf("Atributo inválido! Por favor, escolha um número entre 1 e 5.\n");
+        return 1; // Encerra o programa se o atributo for inválido
 }
+printf("====Resultado Final:====\n");
+printf("Soma dos atributos da carta 1: %.2f\n", soma_carta1);
+printf("Soma dos atributos da carta 2: %.2f\n", soma_carta2);
+printf("%s\n", 
+    (soma_carta1 > soma_carta2) ? "A carta 1 venceu a batalha final!" :
+    (soma_carta1 < soma_carta2) ? "A carta 2 venceu a batalha final!" :
+    "As cartas empataram na batalha final!");
 
-
-printf("Fim da rodada!\n");
 
 
 return 0;
-
-
 
 
 }
